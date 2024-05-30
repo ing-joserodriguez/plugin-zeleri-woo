e11e4c82c56b8618043c92bff821aaa272d019b9
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-05-30 01:11:56 -0400

    Se agrego zeleri como metodo de pago

---
+++ includes/class-zeleri-woo-oficial-payment-gateways.php
@@ -0,0 +1,15 @@
+<?php
+class Zeleri_Woo_Oficial_Payment_Gateways extends WC_Payment_Gateway {
+    public function __construct() {
+        $this->id = 'zeleri_woo_oficial_payment_gateways';
+        $this->method_title = 'Zeleri';
+        $this->title = 'Zeleri';
+        $this->has_fields = false;
+        // Otros ajustes y configuraciones aquí
+    }
+
+    public function process_payment($order_id) {
+        // Lógica para procesar el pago
+        // Puedes redirigir al usuario a una página de confirmación o procesar el pago directamente
+    }
+}
