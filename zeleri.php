9c5c0c73b0ad6de4012e2db32356465db1509266
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-05-30 01:25:23 -0400

    ajuste para lograr agregar zeleri como pasarela de pago

--- zeleri.php
+++ zeleri.php
@@ -76,7 +76,8 @@ if (stripos(implode($all_plugins), 'woocommerce.php')) {
  * Check if WooCommerce is active
  */
 if ( $woocommerce_is_present ) {
-
+	// En tu-plugin.php
+	require_once plugin_dir_path(__FILE__) . 'includes/class-zeleri-woo-oficial-payment-gateways.php';
 	function add_zeleri_woo_oficial_payment_gateways( $gateways ) {
         $gateways[] = 'Zeleri_Woo_Oficial_Payment_Gateways';
         return $gateways;
