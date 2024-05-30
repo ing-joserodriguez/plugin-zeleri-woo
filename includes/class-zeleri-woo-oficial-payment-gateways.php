e18b868785756306a4d7235b296f44f407230a4a
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-05-30 01:34:03 -0400

    se agrega uans configuracion al nuevo metodo de pago zeleri

--- includes/class-zeleri-woo-oficial-payment-gateways.php
+++ includes/class-zeleri-woo-oficial-payment-gateways.php
@@ -4,9 +4,29 @@ class Zeleri_Woo_Oficial_Payment_Gateways extends WC_Payment_Gateway {
         $this->id = 'zeleri_woo_oficial_payment_gateways';
         $this->method_title = 'Zeleri';
         $this->title = 'Zeleri';
-        $this->has_fields = false;
+        $this->has_fields = true;
         // Otros ajustes y configuraciones aquí
     }
+
+    public function display_settings() {
+      $settings_url = admin_url('admin.php?page=zeleri');
+      wp_redirect($settings_url);
+      exit;
+    }
+
+    public function init_form_fields() {
+      $this->form_fields = array(
+          'enabled' => array(
+              'title' => 'Habilitar Zeleri',
+              'type' => 'checkbox',
+              'label' => 'Habilitar este método de pago',
+              'default' => 'yes',
+          ),
+          // Otros campos aquí
+      );
+    }
+  
+  
 
     public function process_payment($order_id) {
         // Lógica para procesar el pago
