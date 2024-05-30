eeaef809ac6ffb8776b146e264c04deea8682b27
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-05-30 01:53:09 -0400

    ajustes metodo zeleri de pago

--- includes/class-zeleri-woo-oficial-payment-gateways.php
+++ includes/class-zeleri-woo-oficial-payment-gateways.php
@@ -4,30 +4,17 @@ class Zeleri_Woo_Oficial_Payment_Gateways extends WC_Payment_Gateway {
         $this->id = 'zeleri_woo_oficial_payment_gateways';
         $this->method_title = 'Zeleri';
         $this->title = 'Zeleri';
-        $this->has_fields = true;
-        // Otros ajustes y configuraciones aquí
+        $this->has_fields = false;
     }
 
-    public function display_settings() {
-      $settings_url = admin_url('admin.php?page=zeleri');
-      wp_redirect($settings_url);
-      exit;
-    }
-
-    public function init_form_fields() {
-      $this->form_fields = array(
-          'enabled' => array(
-              'title' => 'Habilitar Zeleri',
-              'type' => 'checkbox',
-              'label' => 'Habilitar este método de pago',
-              'default' => 'yes',
-          ),
-          // Otros campos aquí
-      );
+    public function admin_options() {
+      echo '<h2>' . esc_html( $this->get_method_title() );
+      wc_back_link( __( 'Return to payments', 'woocommerce' ), admin_url( 'admin.php?page=zeleri' ) );
+      echo '</h2>';
+      echo wp_kses_post( wpautop( $this->get_method_description() ) );
+      parent::admin_options();
     }
   
-  
-
     public function process_payment($order_id) {
         // Lógica para procesar el pago
         // Puedes redirigir al usuario a una página de confirmación o procesar el pago directamente
