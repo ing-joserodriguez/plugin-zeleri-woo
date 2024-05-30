459b7294072f2685ab8813b8124c1fc616ce11dd
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-05-30 00:13:31 -0400

    Ajuste en los contenidos de las secciones de menu principal

--- admin/partials/zeleri-admin-display.php
+++ admin/partials/zeleri-admin-display.php
@@ -36,40 +36,54 @@
 <div class="container p-5 my-5 border">
 	<!-- <h1><?php //echo esc_html( get_admin_page_title() ); ?></h1> -->
 	
- 	<!-- Nav tabs -->
-	<ul class="nav nav-pills nav-stacked" role="tablist">
-		<li class="nav-item">
-			<a class="nav-link active" data-bs-toggle="tab" href="#home">Inicio</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" data-bs-toggle="tab" href="#setting">Configuracion</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" data-bs-toggle="tab" href="#help">Centro de ayuda</a>
-		</li>
-	</ul>
+ 		<!-- Nav tabs -->
+	<div class="row">
+    
+	
+		<div class="col-md-3">
+			<ul class="nav flex-column">
+				<li class="nav-item">
+					<a class="nav-link active" data-bs-toggle="pill" href="#home">Inicio</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" data-bs-toggle="pill" href="#settings">Configuracion</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" data-bs-toggle="pill" href="#transactions">Transacciones</a>
+				</li>
+			</ul>
+		</div>
 
-  	<!-- Tab panes -->
-  	<div class="tab-content">
-		<div id="home" class="tab-pane fade in active">
-			<div class="section-tab-home d-flex flex-column align-items-center">
-				<img src="http://localhost/wordpress/wp-content/uploads/2023/10/Z-logo.png" alt="Logo Zeleri" />
-				<h5>Te damos la bienvenida a Zeleri para WordPress</h5>
-				<p>Con el plugin oficial de Zeleri para WordPress podrás realizar tus pagos, <br> de manera facil, rapida y segura.</p>
+
+		<div class="col-md-9">
+			<div class="tab-content">
+				
+				<div class="tab-pane fade show active" id="home">
+					<div class="section-tab-home d-flex flex-column align-items-center">
+						<img src="http://localhost/wordpress/wp-content/uploads/2023/10/Z-logo.png" alt="Logo Zeleri" />
+						<h5>Te damos la bienvenida a Zeleri para WordPress</h5>
+						<p>Con el plugin oficial de Zeleri para WordPress podrás realizar tus pagos, <br> de manera facil, rapida y segura.</p>
+					</div>
+				</div>
+
+				<div class="tab-pane fade" id="settings">
+					<form action='options.php' method='post'>
+						<?php
+							settings_fields( 'pluginPage' );
+							do_settings_sections( 'pluginPage' );
+							submit_button();
+						?>
+					</form>
+				</div>
+
+				<div class="tab-pane fade" id="transactions">
+					<h3>Centro de ayuda</h3>
+					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+				</div>
+				
 			</div>
 		</div>
-		<div id="setting" class="tab-pane fade"><br>
-			<form action='options.php' method='post'>
-				<?php
-					settings_fields( 'pluginPage' );
-					do_settings_sections( 'pluginPage' );
-					submit_button();
-				?>
-			</form>
-		</div>
-		<div id="help" class="tab-pane fade"><br>
-			<h3>Centro de ayuda</h3>
-			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-		</div>
+
 	</div>
+
 </div>
