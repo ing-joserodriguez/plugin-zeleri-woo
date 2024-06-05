9d3b7cc41ae0214f2836eed1ec8390b301de2da2
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-05 10:23:16 -0400

    reestructuracion menu principal

--- admin/partials/zeleri-admin-display.php
+++ admin/partials/zeleri-admin-display.php
@@ -34,64 +34,45 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="container p-5 my-5 border">
-	<!-- <h1><?php //echo esc_html( get_admin_page_title() ); ?></h1> -->
-	
- 		<!-- Nav tabs -->
-	<div class="row">
-    
-	
-		<div class="col-md-3">
-			<ul class="nav flex-column">
+	<!--<div class="container"> -->
+			<div class="row">
+				<div class="col-md-3">
+					<ul class="nav nav-tabs flex-column">
 				<li class="nav-item">
-					<a class="nav-link active" data-bs-toggle="pill" href="#home">Inicio</a>
+					<a class="nav-link" data-toggle="tab" href="#inicio">Inicio <i class="ph-bold ph-caret-right"></i></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" data-bs-toggle="pill" href="#transactions">Transacciones</a>
+					<a class="nav-link" data-toggle="tab" href="#transacciones">Transacciones <i class="ph-bold ph-caret-right"></i></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" data-bs-toggle="pill" href="#settings">Configuracion</a>
+					<a class="nav-link active" data-toggle="tab" href="#configuracion">Configuración <i class="ph-bold ph-caret-right"></i></a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" data-bs-toggle="pill" href="#ayuda">Ayuda</a>
-				</li>
-			</ul>
-		</div>
+					</ul>
+				</div>
+				<div class="col-md-9">
+					<div class="tab-content">
+						<div id="inicio" class="tab-pane fade">
+							<p>Texto de prueba para la sección "Inicio".</p>
+						</div>
 
+						<div id="transacciones" class="tab-pane fade">
+							<p>Texto de prueba para la sección "Transacciones".</p>
+						</div>
 
-		<div class="col-md-9">
-			<div class="tab-content">
-				
-				<div class="tab-pane fade show active" id="home">
-					<div class="section-tab-home d-flex flex-column align-items-center">
-						<img src="http://localhost/wordpress/wp-content/uploads/2023/10/Z-logo.png" alt="Logo Zeleri" />
-						<h5>Te damos la bienvenida a Zeleri para WordPress</h5>
-						<p>Con el plugin oficial de Zeleri para WordPress podrás realizar tus pagos, <br> de manera facil, rapida y segura.</p>
+						<div id="configuracion" class="tab-pane fade show active">
+							<form action='options.php' method='post'>
+								<?php
+									settings_fields( 'pluginPage' );
+									do_settings_sections( 'pluginPage' );
+									submit_button();
+								?>
+							</form>
+						</div>
 					</div>
 				</div>
-
-				<div class="tab-pane fade" id="transactions">
-					<h3>Transacciones</h3>
-					<p>Tabla de trasacciones.</p>
-				</div>
-
-				<div class="tab-pane fade" id="settings">
-					<form action='options.php' method='post'>
-						<?php
-							settings_fields( 'pluginPage' );
-							do_settings_sections( 'pluginPage' );
-							submit_button();
-						?>
-					</form>
-				</div>
-
-				<div class="tab-pane fade" id="transactions">
-					<h3>Ayuda</h3>
-					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-				</div>
-				
 			</div>
 		</div>
 
-	</div>
-
+	<!--</div>-->
 </div>
+
