ae80f8481c656cdfce9e8acd20d681beff869b62
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-05 22:42:49 -0400

    ajustes pro defecto de los metodos de pago

--- includes/class-zeleri-woo-oficial-payment-gateways.php
+++ includes/class-zeleri-woo-oficial-payment-gateways.php
@@ -9,12 +9,12 @@ class Zeleri_Woo_Oficial_Payment_Gateways extends WC_Payment_Gateway {
         $options = get_option( 'zeleri_settings' );
 
         $this->enabled = ( isset($options['zeleri_checkbox_field_0']) ) ? $options['zeleri_checkbox_field_0'] : 'no';
-        $this->method_description  = ( isset($options['zeleri_textarea_field_4']) ) ? $options['zeleri_textarea_field_4'] : 'Descripcion por defecto';
+        $this->method_description  = ( isset($options['zeleri_textarea_field_4']) ) ? $options['zeleri_textarea_field_4'] : '';
     }
   
     public function process_payment($order_id) {
-    global $woocommerce;
-    $order = new WC_Order( $order_id );
+        global $woocommerce;
+        $order = new WC_Order( $order_id );
         // Lógica para procesar el pago
         // Puedes redirigir al usuario a una página de confirmación o procesar el pago directamente
     }
