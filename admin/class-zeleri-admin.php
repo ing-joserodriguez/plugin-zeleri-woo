57b122f252a8b2d8261f300f5d2790138145dbec
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-07 00:44:35 -0400

    ajustes

--- admin/class-zeleri-admin.php
+++ admin/class-zeleri-admin.php
@@ -131,7 +131,6 @@ class Zeleri_Admin {
 	public function woocommerceZeleriInit() {
 		add_action(
 			'admin_notices',
-			$plugin_admin,
 			function () {
 					$zeleriLogo = sprintf('%s%s', dirname(plugin_dir_url(__FILE__)), '/admin/images/ze-logo-131.png');
 					$this->template->render('public/notices/review-notice.php', [
