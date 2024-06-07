8ae562502929dfb1e4b557483b6feaab5737a4b1
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-07 01:26:17 -0400

    ajustes

--- admin/class-zeleri-admin.php
+++ admin/class-zeleri-admin.php
@@ -135,7 +135,7 @@ class Zeleri_Admin {
     echo '<div class="notice notice-info is-dismissible" id="zeleri-review-notice">
         <div class="zeleri-notice">
             <div>
-                <img src="' . esc_url( plugin_dir_url( __FILE__ ) ) . '/admin/images/ze-logo-131.png" height="70px" alt="Zeleri logo" />
+                <img src="' . esc_url( plugin_dir_url( __FILE__ ) ) . 'images/ze-logo-131.png" height="70px" alt="Zeleri logo" />
             </div>
             <div class="zeleri-review-text">
                 <p class="zeleri-review-title">Tu opinión es importante para nosotros</p>
