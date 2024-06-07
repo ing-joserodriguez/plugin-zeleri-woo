51c453c42fa46c7b5189d25f2482679a71678546
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-07 00:57:47 -0400

    ajustes

--- admin/class-zeleri-admin.php
+++ admin/class-zeleri-admin.php
@@ -129,16 +129,11 @@ class Zeleri_Admin {
 	}
 
 	public function woocommerceZeleriInit() {
-		add_action(
-			'admin_notices',
-			function () {
-				include 'zeleri/public/notices/review-notice.php';
-			}
-		);
+		add_action('admin_notices', 'review_notice');
 	}
 		
 	public function index() {
-		//include 'partials/zeleri-admin-display.php';
+		// CODE HERE
 	}
 
 	public function zeleri_woo_oficial_settings_view() {
@@ -300,5 +295,28 @@ class Zeleri_Admin {
 		<?php
 	
 	}*/
+
+	public function review_notice() { 
+		?>
+		<div class="notice notice-info is-dismissible" id="zeleri-review-notice">
+			<div class="zeleri-notice">
+					<div>
+							<img src="<?= sprintf('%s%s', dirname(plugin_dir_url(__FILE__)), '/admin/images/ze-logo-131.png'); ?>" height="70px" alt="Zeleri logo" />
+					</div>
+					<div class="zeleri-review-text">
+							<p class="zeleri-review-title">Tu opinión es importante para nosotros</p>
+							<p>¿Podrías tomarte un momento para dejarnos una reseña en el repositorio de WordPress?
+									Solo te tomará un par de minutos
+									y nos ayudará a seguir mejorando y llegar a más personas como tú.</p>
+					</div>
+					<a class="button button-primary zeleri-button-primary"
+							href="https://wordpress.org/support/plugin/transbank-webpay-plus-rest/reviews/#new-post"
+							target="_blank" rel="noopener"
+					>Dejar reseña</a>
+			</div>
+		</div>
+		<?php
+	
+	}
 
 }
