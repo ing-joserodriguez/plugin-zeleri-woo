ccaf773b8fa5cb2e9ff0ec155ea2418fd800f956
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-07 02:59:08 -0400

    ajustes

--- admin/partials/zeleri-admin-display.php
+++ admin/partials/zeleri-admin-display.php
@@ -18,23 +18,6 @@
 	if ( ! current_user_can( 'manage_options' ) ) {
 		return;
 	}
-	var_dump($_GET);
-	if ( isset( $_GET['tab_active'] ) ) {
-		$tab = 'settings';
-	}
-	else{
-			$tab = 'home';
-	}
-
-	function zeleri_is_nav_active($tab, $val, $sec = '') {
-			if ($tab === $val && $sec === '') {
-					echo 'active';
-			}
-
-			if ($tab === $val && $sec !== '') {
-				echo 'show active';
-		}
-	}
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
@@ -44,33 +27,33 @@
 				<div class="col-md-2">
 					<ul class="nav nav-tabs flex-column">
 				<li class="nav-item">
-					<a class="nav-link <?php zeleri_is_nav_active($tab, 'home'); ?>" data-toggle="tab" href="#inicio">Inicio <i class="ph-bold ph-caret-right"></i></a>
+					<a class="nav-link active" data-toggle="tab" href="#inicio">Inicio <i class="ph-bold ph-caret-right"></i></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link <?php zeleri_is_nav_active($tab, 'transactions'); ?>" data-toggle="tab" href="#transacciones">Transacciones <i class="ph-bold ph-caret-right"></i></a>
+					<a class="nav-link" data-toggle="tab" href="#transacciones">Transacciones <i class="ph-bold ph-caret-right"></i></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link <?php zeleri_is_nav_active($tab, 'settings'); ?>" data-toggle="tab" href="#configuracion">Configuración <i class="ph-bold ph-caret-right"></i></a>
+					<a class="nav-link" data-toggle="tab" href="#configuracion">Configuración <i class="ph-bold ph-caret-right"></i></a>
 				</li>
 					</ul>
 				</div>
 				<div class="col-md-7">
 					<div class="tab-content">
-						<div id="inicio" class="tab-pane fade  <?php zeleri_is_nav_active($tab, 'home', 'content'); ?>">
+						<div id="inicio" class="tab-pane fade show active">
 							<p>Texto de prueba para la sección "Inicio".</p>
 						</div>
 
-						<div id="transacciones" class="tab-pane fade <?php zeleri_is_nav_active($tab, 'transactions', 'content'); ?>">
+						<div id="transacciones" class="tab-pane fade">
 							<p>Texto de prueba para la sección "Transacciones".</p>
 						</div>
 
-						<div id="configuracion" class="tab-pane fade <?php zeleri_is_nav_active($tab, 'settings', 'content'); ?>">
+						<div id="configuracion" class="tab-pane fade">
 							<?php $this->generate_settings_html(); ?>
 
 							<p class="submit zeleri-button-submit">
 								<button name="save" class="button-primary woocommerce-save-button" type="submit" value="Guardar los cambios">Guardar los cambios</button>
 								<input type="hidden" id="_wpnonce" name="_wpnonce" value="3e1ecd8a35">
-								<input type="hidden" name="_wp_http_referer" value="/wp-admin/admin.php?page=wc-settings&amp;tab=checkout&amp;section=zeleri_woo_oficial_payment_gateways&amp;tab_active=settings">		
+								<input type="hidden" name="_wp_http_referer" value="/wp-admin/admin.php?page=wc-settings&amp;tab=checkout&amp;section=zeleri_woo_oficial_payment_gateways">		
 							</p>
 						</div>
 					</div>
