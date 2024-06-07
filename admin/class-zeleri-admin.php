d55bcc61ac0cb9430f128329e716fc8ac53f6acf
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-07 03:02:17 -0400

    ajustes

--- admin/class-zeleri-admin.php
+++ admin/class-zeleri-admin.php
@@ -73,7 +73,7 @@ class Zeleri_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/zeleri-admin.css', array(), '1.1.6', 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/zeleri-admin.css', array(), $this->version, 'all' );
 
 		wp_enqueue_style( 'Bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
 
--- includes/class-template.php
+++
@@ -1,7 +0,0 @@
-<?php
-  class Template {
-    const TEMPLATE_PATH = WP_PLUGIN_DIR.'/zeleri/public/';
-    public function render(string $name, array $parameters): void {
-        wc_get_template( $name, $parameters, null, self::TEMPLATE_PATH );
-    }
-}
