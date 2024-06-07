5d26a39bbda44bce03be8ad2c4f7a458d9060c3b
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-07 00:49:30 -0400

    ajustes

--- admin/class-zeleri-admin.php
+++ admin/class-zeleri-admin.php
@@ -132,10 +132,11 @@ class Zeleri_Admin {
 		add_action(
 			'admin_notices',
 			function () {
-					$zeleriLogo = sprintf('%s%s', dirname(plugin_dir_url(__FILE__)), '/admin/images/ze-logo-131.png');
+				include dirname(plugin_dir_url(__FILE__)), '/public/notices/review-notice.php');
+					/*$zeleriLogo = sprintf('%s%s', dirname(plugin_dir_url(__FILE__)), '/admin/images/ze-logo-131.png');
 					$this->template->render('public/notices/review-notice.php', [
 							'zeleriLogo' => esc_url($zeleriLogo)
-					]);
+					]);*/
 			}
 		);
 	}
