690da5ba3e4c0df1f5d93ee2b70313ef39a1addd
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-12 13:04:14 -0400

    ajustes

--- admin/partials/zeleri-admin-display.php
+++ admin/partials/zeleri-admin-display.php
@@ -164,7 +164,7 @@
 
 							<p class="submit zeleri-button-submit">
 								<?php 
-									$nonce = wp_create_nonce('wp_update');
+									$nonce = wp_create_nonce('wp_rest');
 									$isVerify = wp_verify_nonce($nonce);
 									var_dump($isVerify);
 								?>
