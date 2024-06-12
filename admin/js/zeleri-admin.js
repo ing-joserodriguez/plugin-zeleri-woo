622e50e6f1007159ad802b6292b3c293683faff2
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-12 15:03:42 -0400

    ajustes

--- admin/js/zeleri-admin.js
+++ admin/js/zeleri-admin.js
@@ -46,18 +46,6 @@
         const IDTabActive = "#" + params.get("tab_pane");
         $('.nav-tabs a[href="' + IDTabActive + '"]').trigger("click");
       }
-
-      //Valido si la variable "tab_pane" viene en la url
-      //if (params.get("tab_pane") == "tabZeleriTransacciones") {
-      //Si existe, se simulamuestra la seccion en donde se estaba anteriormente
-      //$('.nav-tabs a[href="#tabZeleriTransacciones"]').trigger("click");
-      //}
-
-      //Valido si la variable "tab_pane" viene en la url
-      //if (params.get("tab_pane") == "tabZeleriConfiguracion") {
-      //Si existe, se simulamuestra la seccion en donde se estaba anteriormente
-      //$('.nav-tabs a[href="#tabZeleriConfiguracion"]').trigger("click");
-      //}
     }
 
     //Hace que el enlace de la seccion de inicio despliegue la seccion de las configuraciones
