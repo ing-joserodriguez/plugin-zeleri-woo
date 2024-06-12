af2378f1f7260328cdeea374858510728b234176
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-12 12:24:16 -0400

    ajustes

--- admin/partials/zeleri-admin-display.php
+++ admin/partials/zeleri-admin-display.php
@@ -163,11 +163,8 @@
 							<?php $this->generate_settings_html(); ?>
 
 							<p class="submit zeleri-button-submit">
-								<?php 
-									//$nonce = wp_create_nonce();
-								?>
 								<button name="save" class="button-primary woocommerce-save-button" type="submit" value="Guardar los cambios">Guardar los cambios</button>
-								<input type="hidden" id="_wpnonce" name="_wpnonce" value="<?php echo esc_attr( wp_create_nonce() ); ?>" />
+								<input type="hidden" id="_wpnonce" name="_wpnonce" value="<?php echo esc_attr( wp_create_nonce('save_post') ); ?>" />
 								<input type="hidden" name="_wp_http_referer" value="/wp-admin/admin.php?page=wc-settings&amp;tab=checkout&amp;section=zeleri_woo_oficial_payment_gateways">		
 							</p>
 						</div>
