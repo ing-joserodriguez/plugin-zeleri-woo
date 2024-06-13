1cccb0fa96a31d291626638c96cbeb5fe22557d9
Jose Rodriguez <xjoserodriguezx@gmail.com>
2024-06-12 20:38:29 -0400

    ajustes

--- includes/class-zeleri-woo-oficial-api.php
+++ includes/class-zeleri-woo-oficial-api.php
@@ -61,17 +61,11 @@ if ( ! class_exists( 'Zeleri_Woo_Oficial_API' ) ) {
 		private function do_remote_post($url, $_token_customer, $payload){
 			return wp_remote_post( $url, array(
 					'method' => 'POST',
-					'timeout' => 190,
-					'redirection' => 5,
-					'httpversion' => '1.0',
-					'blocking' => true,
 					'headers' => array(
 						'Content-Type' => 'application/json',
-							'Authorization: Bearer ' => $_token_customer
+						'Authorization: Bearer ' => $_token_customer
 					),
-					'body' => json_encode($payload),
-					'cookies' => array(),
-					'sslverify' => FALSE
+					'body' => json_encode($payload)
 			  )
 			);
 		}
